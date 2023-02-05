1d9d0f1262cdd3ad637322f0b8225ca1e2bbdac5
takaaki <takaaking26@gmail.com>
2023-02-05 11:49:50 +0900

    更新機能を追加、完了時一覧削除を追加

--- src/main/java/com/example/todo_psql/app/mapper/TodoMapper.java
+++ src/main/java/com/example/todo_psql/app/mapper/TodoMapper.java
@@ -7,6 +7,9 @@ import com.example.todo_psql.app.entity.Todo;
 @Mapper
 public interface TodoMapper {
     public List<Todo> selectAll();
-
+    public List<Todo> selectIncomplete();
+    public List<Todo> selectComplete();
     public void add(Todo todo);
+    public void update(Todo todo);
+    public void delete();
 }
